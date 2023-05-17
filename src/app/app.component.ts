2ed1655dd8699a6455b3b9c4357b9cbc2cd666dc
nestor Maldonado <nestorjmaldo@gmail.com>
2023-05-17 20:30:16 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title: string = 'Primera App en Angular';
+  public title: string = 'Aplicaciones en Angular';
  
 }
